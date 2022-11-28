b8ec6c2f676791c4ec8f877b5d91c850596f4d6e
Joey Tsui <joeytsui131@gmail.com>
2022-11-28 14:08:29 -0500

    "1"

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,23 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
+    def initialize (value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
 
-end
+    def parent=(parent)
+        if @parent != nil
+            @parent.children.delete(self)
+            @parent = parent
+            @parent.children << self
+        else
+            @parent = parent
+            @parent.children << self
+        end
+    end
+
+    def add_child(child)
+        
+    end
+end 
